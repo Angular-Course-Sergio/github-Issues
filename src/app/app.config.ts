077eccc1ca4442b801cd58f70d7e07298f092a19
Sergio Barreras <sergiobg.isc@gmail.com>
2024-09-05 08:32:11 -0700

    feat: configuración de tanstack query

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,19 @@
 import {
   ApplicationConfig,
   provideExperimentalZonelessChangeDetection,
-  provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import {
+  provideAngularQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideExperimentalZonelessChangeDetection(),
+    provideAngularQuery(new QueryClient()),
   ],
 };
