8e4cc3c9dc54eb4f5cf42ead8b2e536c22b37cbd
Sergio Barreras <sergiobg.isc@gmail.com>
2024-09-10 14:17:14 -0700

    feat: prefetch query data

--- src/app/modules/issues/components/issue-item/issue-item.component.ts
+++ src/app/modules/issues/components/issue-item/issue-item.component.ts
@@ -1,7 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { GithubIssue, State } from '../../interfaces';
 import { CommonModule } from '@angular/common';
+import { IssueService } from '../../services/issue.service';
 
 @Component({
   selector: 'issue-item',
@@ -11,8 +12,11 @@ import { CommonModule } from '@angular/common';
 })
 export class IssueItemComponent {
   issue = input.required<GithubIssue>();
+  issueService = inject(IssueService);
 
   get isOpen() {
     return this.issue().state === State.Open;
   }
+  prefetchData = () =>
+    this.issueService.prefetchIssue(this.issue().number.toString());
 }
